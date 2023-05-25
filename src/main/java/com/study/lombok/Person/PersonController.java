970a3f2d04c066ba972b5e573ed28e314acdc3da
lucasalvess96 <lucasalves.57@hotmail.com>
2023-05-24 22:01:45 -0300

    fix: ajuste ao salvar pessoa

--- src/main/java/com/study/lombok/Person/PersonController.java
+++ src/main/java/com/study/lombok/Person/PersonController.java
@@ -3,7 +3,6 @@ package com.study.lombok.Person;
 import com.study.lombok.Person.Dto.PersonCreateDto;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +21,6 @@ public class PersonController {
     @Transactional
     public ResponseEntity<PersonCreateDto> create(@RequestBody @Valid PersonCreateDto personCreateDto){
         PersonCreateDto createDto = personService.createPerson(personCreateDto);
-        if (createDto != null) return new ResponseEntity<>(createDto, HttpStatus.CREATED);
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        return ResponseEntity.ok().body(createDto);
     }
 }
